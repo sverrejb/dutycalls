e062f1db28017c5752a15ac6d318b979a5dcdab8
kyrrelm <kyrrelm@stund.ntnu.no>
2012-04-17 17:29:33 +0200

    Kyrre

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -3,5 +3,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Hello Kyrre! :D");
+		System.out.println("Kyrre");
 	}
 }
