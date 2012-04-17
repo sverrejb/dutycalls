2d4e6c33bc8ca6593118a6c3e8a0ca1f62d0ce86
kyrrelm <kyrrelm@stund.ntnu.no>
2012-04-17 20:28:25 +0200

    MainMenu fixes

--- src/Incognito/MainMenu.java
+++ src/Incognito/MainMenu.java
@@ -12,26 +12,34 @@ import org.newdawn.slick.tiled.TiledMap;
 public class MainMenu extends BasicGameState {
 	
 	Image background = null;
-	private int stateID = -1;
+	Image startGameOption = null;
+	Image exitOption = null;
 	private String versionName = "Incognito InDev";
+	private int stateID = -1;
+	private float menuX = 120;
+	private float menuY = 160;
 	
 	public MainMenu(int stateID) {
 		// TODO Auto-generated constructor stub
 		this.stateID = stateID;
-		
-		//System.out.println(GL11.glGetInteger(GL11.GL_MAX_TEXTURE_SIZE));
 	}
 
 	@Override
 		// TODO Auto-generated method stub
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
 		background = new Image("img/background.png");
+		Image menuOptions = new Image("img/menuOptions.png");
+		startGameOption = menuOptions.getSubImage(0, 0, 377, 71); 
+		exitOption = menuOptions.getSubImage(0, 71, 377, 71);
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
 		background.draw(0, 0);
 		g.drawString(versionName, 600, 10);
+		
+		startGameOption.draw(menuX, menuY, 1); 
+		exitOption.draw(menuX, menuY+80, 1);
 	}
 
 	@Override
