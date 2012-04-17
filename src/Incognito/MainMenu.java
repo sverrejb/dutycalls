c272a1a9d41bab49f06d91256c5632e7b631e6b1
Per Oyvind Kanestroem <perok@stud.ntnu.no>
2012-04-17 19:07:31 +0200

    Tiles testing

--- src/Incognito/MainMenu.java
+++ src/Incognito/MainMenu.java
@@ -5,20 +5,21 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.tiled.TiledMap;
 
 public class MainMenu extends BasicGameState {
+	
+	TiledMap map;
 
 	@Override
-	public void init(GameContainer arg0, StateBasedGame arg1)
-			throws SlickException {
-		// TODO Auto-generated method stub
+	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException{
+		map = new TiledMap("Resources/untitled.tmx", "Resources");
 		
 	}
 
 	@Override
-	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2)
-			throws SlickException {
-		// TODO Auto-generated method stub
+	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) throws SlickException{
+		map.render(0, 0);
 		
 	}
 
