77b22f0b5d5fc33cdafaaf3486c9f7b810a2db5c
kyrrelm <kyrrelm@stund.ntnu.no>
2012-04-18 16:51:57 +0200

    sound

--- src/Incognito/MainMenu.java
+++ src/Incognito/MainMenu.java
@@ -5,6 +5,7 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -73,11 +74,11 @@ public class MainMenu extends BasicGameState {
 			    StartbuttonScale += scaleStep * delta;
 			 
 			  if ( input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON) ){
-			    stateBasedGame.enterState(GameState.GAMEPLAYSTATE);
+				  stateBasedGame.enterState(GameState.GAMEPLAYSTATE);
 			  }
 			}else{
 			  if(StartbuttonScale > 1.0f)
-			    StartbuttonScale -= scaleStep * delta;
+				  StartbuttonScale -= scaleStep * delta;
 			}
 			 
 			if(insideExit)
