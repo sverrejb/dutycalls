81782b9d0e6df2a668730780bf5046357c5b617b
Per Oyvind Kanestroem <perok@stud.ntnu.no>
2012-04-18 17:28:26 +0200

    GamePlay modification

--- src/Incognito/Gameplay.java
+++ src/Incognito/Gameplay.java
@@ -11,16 +11,26 @@ public class Gameplay extends BasicGameState{
 	private int stateID = -1;
 	
 	private PlayerObject player;
+	private GroundObject ground;
+	
+	//Enemy i list
+	
+	//våpen i list
+	
+	//skudd i list
 
 	public Gameplay(int stateID) {
 		this.stateID = stateID;
+		
 		player = new PlayerObject();
+		ground = new GroundObject();
 	}
 
 	@Override
 	public void init(GameContainer gameContainer, StateBasedGame stateBasedGame)
 			throws SlickException {
 		player.init(gameContainer, stateBasedGame);
+		ground.init(gameContainer, stateBasedGame);
 		
 		
 	}
@@ -28,13 +38,29 @@ public class Gameplay extends BasicGameState{
 	@Override
 	public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics g)
 			throws SlickException {
+		//Background
+		ground.render(gameContainer, stateBasedGame, g);
 		player.render(gameContainer, stateBasedGame, g);
+		//enemies
+		//weapons
+		//shots
 		
 	}
 
 	@Override
 	public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta)
 			throws SlickException {
+		
+
+		
+		//Hente keyboard input
+		//--> gi til player
+		
+		//legge til gravitasjon
+		
+		//sjekke for kollisjon
+		
+		ground.update(gameContainer, stateBasedGame, delta);
 		player.update(gameContainer, stateBasedGame, delta);
 		
 	}
--- src/Incognito/GroundObject.java
+++ src/Incognito/GroundObject.java
@@ -1,14 +1,34 @@
 package Incognito;
 
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.tiled.TiledMap;
 
 /*
  * Controls the ground
  */
-public class GroundObject {
+public class GroundObject extends GameObject {
 	private TiledMap ground;
 	
 	public GroundObject() {
+		super();
+	}
+	
+	
+	@Override
+	public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException{
+		ground = new TiledMap("tiles/testLevel.tmx");
+		
+	}
+	@Override
+	public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics g) throws SlickException{
+		ground.render(0, 0);
+	}
+	
+	@Override
+	public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta) throws SlickException {
 		
 	}
 }
