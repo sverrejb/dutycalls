20c4b8564216736cea4db9fa311546267a988c06
kyrrelm <kyrrelm@stund.ntnu.no>
2012-04-27 22:51:58 +0200

    Fixed walking animation. Player now "walks" after doing a jump, or after falling down.

--- src/it/marteEngine/Camera.java
+++ src/it/marteEngine/Camera.java
@@ -21,6 +21,7 @@ public class Camera {
 	/** coordinates of the top left corner of the camera area in the world */
 	public float cameraX;
 	public float cameraY;
+	public float cameraTweekX = 300;
 	/** width and height of the render area for this camera */
 	private float renderWidth;
 	private float renderHeight;
@@ -73,7 +74,7 @@ public class Camera {
 		if (follow != null
 				&& !moveRect.contains(follow.x + follow.width / 2, follow.y
 						+ follow.height / 2)) {
-			float targetCX = follow.x - (this.renderWidth / 2) + Constants.CAMRA_TWEEK_X;
+			float targetCX = follow.x - (this.renderWidth / 2) + cameraTweekX + Constants.CAMRA_TWEEK_X;
 			float targetCY = follow.y - (this.renderHeight / 2) + Constants.CAMRA_TWEEK_Y;
 			// now smoothly move camera on position cameraX, cameraY to position
 			// targetCX, targetCY, using
