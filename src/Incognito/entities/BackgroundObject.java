9c9ebcbc902275089011de5bbc013e7ac38085b9
Per Oyvind Kanestroem <perok@stud.ntnu.no>
2012-04-27 13:47:26 +0200

    Background scrolling

--- src/Incognito/entities/BackgroundObject.java
+++ src/Incognito/entities/BackgroundObject.java
@@ -5,6 +5,7 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
+import it.marteEngine.ME;
 import it.marteEngine.entity.Entity;
 
 
@@ -12,13 +13,17 @@ public class BackgroundObject extends Entity {
 	
 	private Image image;
 	
+	private float fixPosX = 0f;
+	private float fixPosY = 0f;
+	
 	public BackgroundObject(int x, int y, Image image){
 		super(x,y);
 		
 		this.image = image;
 		
 		setGraphic(image);
-		setHitBox(0, 0, image.getWidth(), image.getHeight(), true);
+		setHitBox(0, 0, image.getWidth(), image.getHeight(), false);
+		collidable = false;
 	}
 	
 	@Override
@@ -28,6 +33,15 @@ public class BackgroundObject extends Entity {
 	
 	@Override
 	public void update(GameContainer gameContainer, int delta) throws SlickException {
+		float camPosX = ME.world.camera.cameraX;
+		float camPosY = ME.world.camera.cameraY;
+		
+		if( image.getWidth() % camPosX == 0)
+			fixPosX = camPosX;
+			
+		x = fixPosX;
+		y = fixPosY;
+			
 		super.update(gameContainer, delta);
 	}
 	
