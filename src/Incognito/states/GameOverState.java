3ed87c8155686d626033dc4a0bbc76ca0909df16
Sverre <Sverre@192.168.2.9>
2012-05-01 18:01:51 +0200

    healthpack and gameoverscreen lololol

--- src/Incognito/states/GameOverState.java
+++ src/Incognito/states/GameOverState.java
@@ -2,18 +2,27 @@ package Incognito.states;
 
 import java.awt.Cursor;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.opengl.ImageData;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
 
+import Incognito.utils.Constants;
 import Incognito.utils.Globals;
 
 import it.marteEngine.World;
 
 public class GameOverState extends World{
+	
+	private Image gameOver = new Image("res/img/gameover.png");
 
-	public GameOverState(int id) {
+	public GameOverState(int id) throws SlickException {
 		super(id);
 	}
 	
@@ -21,7 +30,20 @@ public class GameOverState extends World{
 	public void enter(GameContainer container, StateBasedGame game) throws SlickException{
 		//Globals.ac.setMouseCursor((ImageData) new Cursor(Cursor.DEFAULT_CURSOR), 0, 0);
 		super.enter(container, game);
+	}
+	
+	@Override
+	public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta)throws SlickException {
+		Input input = gameContainer.getInput();
 		
+		if(input.isKeyPressed(input.KEY_SPACE))
+			Globals.game.enterState(Constants.MAINMENU_STATE, new FadeOutTransition(Color.black), new FadeInTransition(Color.black));
+		
+	}
+	
+	@Override
+	public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics g) throws SlickException{
+		gameOver.draw(0,0);
 	}
 
 }
