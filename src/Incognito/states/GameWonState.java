7abf60bd25651e81e7ddf6cb1be3afb6ecf51b8c
kyrrelm <kyrrelm@stund.ntnu.no>
2012-05-02 23:45:05 +0200

    created by

--- src/Incognito/states/GameWonState.java
+++ src/Incognito/states/GameWonState.java
@@ -13,6 +13,7 @@ import org.newdawn.slick.state.transition.FadeOutTransition;
 import Incognito.utils.Constants;
 import Incognito.utils.Globals;
 
+import it.marteEngine.ME;
 import it.marteEngine.World;
 
 public class GameWonState extends World{
@@ -40,5 +41,7 @@ public class GameWonState extends World{
 	@Override
 	public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics g) throws SlickException{
 		gameOver.draw(0,0);
+		g.setColor(Color.white);
+		g.drawString("Created by: Per Øyvind Kanestrøm, Sverre Johann Bjørke and Kyrre Laugerud Moe", 40, Constants.GAME_HEIGHT - 20);
 	}
 }
