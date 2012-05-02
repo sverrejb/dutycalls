27041d4d66d18a9621ea8caf1d57bb0f1df0c1cd
Sverre <Sverre@78.91.21.166>
2012-05-02 20:11:45 +0200

    manifest

--- src/Incognito/IncognitoEntry.java
+++ src/Incognito/IncognitoEntry.java
@@ -17,8 +17,8 @@ public class IncognitoEntry{
 	
 	
 	public static void main(String[] args) throws SlickException {
-		System.setProperty("org.lwjgl.librarypath", new File(new File(System.getProperty("user.dir"), "lib/native"), LWJGLUtil.getPlatformName()).getAbsolutePath());
-		System.setProperty("net.java.games.input.librarypath", System.getProperty("org.lwjgl.librarypath"));
+		//System.setProperty("org.lwjgl.librarypath", new File(new File(System.getProperty("user.dir"), "lib/native"), LWJGLUtil.getPlatformName()).getAbsolutePath());
+		//System.setProperty("net.java.games.input.librarypath", System.getProperty("org.lwjgl.librarypath"));
 		
 		AppGameContainer app = new AppGameContainer(new GameState("Incognito"));
 		Globals.ac = app;
